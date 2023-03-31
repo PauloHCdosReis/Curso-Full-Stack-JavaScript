b4f1bf7a283d6c96d71fc23e47be853e4f08e50b
Paulo Henrique Couto dos Reis <paulohcdosreis@gmail.com>
2023-03-31 13:49:52 -0300

    3

--- Logica de Programacao/Aula46/index.js
+++ Logica de Programacao/Aula46/index.js
@@ -7,4 +7,44 @@ const resultado = calcularMedia(7, 2)
 
 console.log(resultado)
 
-function criarProduto(nome, preco)
+function criarProduto(nome, preco){
+  const produto = {
+    nome,
+    preco,
+    estoque: 1
+  }
+  return produto
+}
+console.log (criarProduto("Notebook Intel 13 Core", 2500))
+
+function areaRetangular(base, altura){
+  return base * altura  
+}
+
+function areaQuadrada(lado){
+  return areaRetangular(lado, lado)
+}
+
+/* console.log(areaRetangular(3,5))
+
+console.log(areaQuadrada(9)) */
+
+function ola(){
+  let texto = ".."
+  return texto
+  texto = "Olá, mundo"
+  console.log(texto)
+}
+
+console.log(ola())
+
+function maioridade(idade){
+  if(idade >= 18){
+    return 'Maior de idade'
+  }else{
+    return "Menor de idade"
+  }
+}
+
+console.log(maioridade(29))
+console.log(maioridade(13))
