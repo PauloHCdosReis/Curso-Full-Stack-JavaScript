6939f3c293fae610de95aa4db0dcc90ae65ace88
Paulo Henrique Couto dos Reis <paulohcdosreis@gmail.com>
2023-07-13 16:33:38 -0300

    Typescript

---
+++ Typescript/Aula06/index.ts
@@ -0,0 +1,11 @@
+const usuario = {
+  name: 'paulo',
+  idade: 18,
+  maiorIdade: true
+}
+
+function cadastro(usuario: { name: string; idade: number; maiorIdade: boolean }, idade: number){
+
+}
+
+cadastro(usuario, 50)
